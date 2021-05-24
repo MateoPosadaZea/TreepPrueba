f0ff9786c0c6d5c0d12a73dd1e5f0a80ec459ba1
MateoPosadaZea <mateo.posadazea@utadeo.edu.co>
2021-05-23 19:17:16 -0500

    Creacion pagina

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import './Footer.css';
+
+
+
+export class Footer extends Component{
+    render(){
+        return(
+           <div>
+               <h1>MATEOOOOOOOO</h1>
+
+           </div>
+
+           
+            
+        )
+    }
+}
