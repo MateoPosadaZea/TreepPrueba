3194eb2cb7d45089eb334e7ff088fe480a8dac9a
MateoPosadaZea <mateo.posadazea@utadeo.edu.co>
2021-06-04 09:00:10 -0500

    Settings

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,36 @@
+import React,{Component} from 'react';
+import './Footer.css';
+
+
+
+export class Footer extends Component{
+    render(){
+        return(
+           <div>
+               <h1>MATEOOOOOOOO</h1>
+
+           </div>
+
+           
+            
+        )
+    }
+}
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Footer(props) {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Footer.propTypes = {
+
+}
+
+export default Footer
+
